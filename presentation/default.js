b8afb677e679ead6e3bd8bb1a83cd2c53bfd8f68
lucamarie <80831645+lucamarie@users.noreply.github.com>
2024-06-01 19:59:44 +0200

    add order popup to configurator pages

--- presentation/default.js
+++ presentation/default.js
@@ -17,35 +17,39 @@ app.get("/", async function (req,res){
     const response = await fetch("http://localhost:81")
     const jsonresponse = await response.json()
     console.log(jsonresponse)
-    res.render("home.ejs", {
+    res.render("home.ejs", { showNavbar: true }, {
         location: jsonresponse
     });
 });
 
 app.get("/models", function (req, res){
-    res.render("models.ejs")
+    res.render("models.ejs", { showNavbar: true })
 })
 
 app.get("/configuratorHyperionHalo", function (req, res){
-    res.render("configuratorHyperionHalo.ejs")
+    res.render("configuratorHyperionHalo.ejs", { showNavbar: true })
 })
 
 app.get("/configuratorTraverse", function (req, res){
-    res.render("configuratorTraverse.ejs")
+    res.render("configuratorTraverse.ejs", { showNavbar: true })
 })
 
+app.get("/orderPopup", function (req, res){
+    res.render("orderPopup.ejs", { showNavbar: false })
+});
+
 app.get("/configurator/:model", function (req, res){
-    res.render("configurator.ejs", {
-        model:  req.params.model
+    res.render("configurator.ejs", { showNavbar: true }, {
+        model:  req.params.model, 
     })
 })
 
 app.get("/testdrive", function (req, res){
-    res.render("testdrive.ejs")
+    res.render("testdrive.ejs", { showNavbar: true })
 })
 
 app.get("/merch", function (req, res){
-    res.render("merchshop.ejs")
+    res.render("merchshop.ejs", { showNavbar: true })
 })
 
 app.listen(PORT, function (err) {
