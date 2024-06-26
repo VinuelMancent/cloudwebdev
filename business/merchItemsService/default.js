6d5c7b0a0bbd66d319b1998c8e2dd66fe1a3b7e1
Vincent <vincentmattes@gmail.com>
2024-06-26 15:04:18 +0200

    merchItemsService now works

--- business/merchItemsService/default.js
+++ business/merchItemsService/default.js
@@ -10,19 +10,20 @@ const PORT = 85
 
 app.get("/", async function (req,res){
     const connection = await mysql.createConnection({
-        host: 'hhzcar',
+        host: 'localhost',
         user: 'hhz',
         password: 'car',
-        database: 'merchItems'
+        database: 'merchandise_db'
     })
     try {
         // Daten aus der Tabelle "testdrive" abrufen
         const [items] = await connection.query('SELECT * FROM merchItems')
-         const merchItems = items.map(row => ({
-            name: items.name,
-            price: items.price,
-            image: items.image
+        const merchItems = items.map(row => ({
+            name: row.name,
+            price: row.price,
+            image: row.image
         }))
+        console.log(merchItems)
         res.setHeader("Access-Control-Allow-Origin", "*")
         res.json(merchItems)
     } catch (error) {
