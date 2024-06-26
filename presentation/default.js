b0c128ee0323b7630ccdcf621b579bdfec0f36ee
Vincent <vincentmattes@gmail.com>
2024-06-26 16:37:39 +0200

    microservices are now not necessary

--- presentation/default.js
+++ presentation/default.js
@@ -14,8 +14,9 @@ const PORT = 80
 
 app.get("/", async function (req,res){
     //get location from microservice
-    const response = await fetch("http://localhost:81")
-    const jsonresponse = await response.json()
+    //const response = await fetch("http://localhost:81")
+    //const jsonresponse = await response.json()
+    const jsonresponse = {"name":"Monaco","lat":43.733334,"long":7.416667,"latmin":43.728334,"latmax":43.738334,"longmin":7.401667000000001,"longmax":7.431667,"link":"https://www.openstreetmap.org/export/embed.html?bbox=7.401667000000001,43.728334,7.431667,43.738334&layer=mapnik"}
     console.log(jsonresponse)
     res.render("home.ejs", { 
         showNavbar: true,
@@ -28,14 +29,16 @@ app.get("/models", function (req, res){
 })
 
 app.get("/configuratorHyperionHalo", async function (req, res){
-    const response = await fetch("http://localhost:86/HyperionHalo")
-    const jsonresponse = await response.json()
+    //const response = await fetch("http://localhost:86/HyperionHalo")
+    //const jsonresponse = await response.json()
+    const jsonresponse = {"colors":[{"value":"Space Grey","image":"/Hyperion_Halo/space_grey.jpg"},{"value":"Cherry Red","image":"/Hyperion_Halo/cherry_red.jpg"},{"value":"Emerald Green","image":"/Hyperion_Halo/emerald_green.jpg"},{"value":"Ice Blue","image":"/Hyperion_Halo/ice_blue.jpg"}],"tires":[{"value":"Sommerreifen","image":""},{"value":"Winterreifen","image":""},{"value":"Sportreifen","image":""}],"rims":[{"value":"Street Style","image":""},{"value":"Urban Chic","image":""},{"value":"Luxury Elite","image":""}]}
     res.render("configuratorHyperionHalo.ejs", { showNavbar: true, configuration: jsonresponse })
 })
 
 app.get("/configuratorTraverse", async function (req, res){
-    const response = await fetch("http://localhost:86/Traverse")
-    const jsonresponse = await response.json()
+    //const response = await fetch("http://localhost:86/Traverse")
+    //const jsonresponse = await response.json()
+    const jsonresponse = {"colors":[{"value":"Space Grey","image":"/Hyperion_Halo/space_grey.jpg"},{"value":"Cherry Red","image":"/Hyperion_Halo/cherry_red.jpg"},{"value":"Emerald Green","image":"/Hyperion_Halo/emerald_green.jpg"},{"value":"Ice Blue","image":"/Hyperion_Halo/ice_blue.jpg"}],"tires":[{"value":"Sommerreifen","image":""},{"value":"Winterreifen","image":""},{"value":"Sportreifen","image":""}],"rims":[{"value":"Street Style","image":""},{"value":"Urban Chic","image":""},{"value":"Luxury Elite","image":""}]}
     res.render("configuratorTraverse.ejs", { showNavbar: true, configuration: jsonresponse })
 })
 
