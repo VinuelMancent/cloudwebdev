33a1b240944af442f191ed0e3ebabeef4f848f09
Vincent <vincentmattes@gmail.com>
2024-06-26 13:48:08 +0200

    shopping cart service works

--- business/shoppingcartService/default.js
+++ business/shoppingcartService/default.js
@@ -3,6 +3,7 @@ const redis = require("redis")
 const app = express()
 app.use(express.static("public"))
 app.use(express.json())
+const cors = require('cors');
 
 const PORT = 83
 
@@ -24,12 +25,25 @@ app.get("/getItems", function (req,res){
         }else{
             allItems = []
         }
+            res.setHeader("Access-Control-Allow-Origin", "*")
             res.send(JSON.stringify(allItems))
         }
     )
 })
 
 app.post("/pushItem", function (req, res){
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
     allItems = []
     console.log(req.body)
     const itemToPush = req.body
@@ -40,6 +54,7 @@ app.post("/pushItem", function (req, res){
         allItems.push(itemToPush)
         redisClient.set("shoppingcartItems", JSON.stringify(allItems))
     })
+    res.setHeader("Access-Control-Allow-Origin", "*")
     res.sendStatus(200)
 })
 
@@ -47,6 +62,13 @@ app.post("clearItems", function (req, res){
     redisClient.set("shoppingcartItems", [])
     res.sendStatus(200)
 })
+
+ const corsOptions = {
+   origin: '*',//(https://your-client-app.com)
+   optionsSuccessStatus: 200,
+ };
+
+app.use(cors(corsOptions))
 
 app.listen(PORT, function (err) {
     if (err) console.log(err)
