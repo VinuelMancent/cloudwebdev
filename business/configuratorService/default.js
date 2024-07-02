578470dbfe25c2c6dcbf344f8096f0b8dacdc97d
Vincent <vincentmattes@gmail.com>
2024-07-02 19:05:55 +0200

    removed old hardcoded values from service

--- business/configuratorService/default.js
+++ business/configuratorService/default.js
@@ -19,7 +19,6 @@ app.get("/:car", async function (req,res){
         database: 'merchandise_db'
     })
     try {
-        // Daten aus der Tabelle "testdrive" abrufen
         const table = car=="HyperionHalo" ? "hyperion":"traverse"
         const query = `SELECT * FROM configurator_${table}`
         const [items] = await connection.query(query)
@@ -31,35 +30,6 @@ app.get("/:car", async function (req,res){
     } catch (error) {
         console.error('Fehler beim Abrufen der Daten:', error)
     }
-
-    /*
-    const tires = [{value: "Sommerreifen", image: ""}, {value: "Winterreifen", image: ""}, {value: "Sportreifen", image: ""}]
-    const rims = [{value: "Street Style", image: ""}, {value: "Urban Chic", image: ""}, {value: "Luxury Elite", image: ""}]
-
-    const HyperionHalo = {
-        colors: [
-            {value: "Space Grey", image: "/Hyperion_Halo/space_grey.jpg"},
-            {value: "Cherry Red", image: "/Hyperion_Halo/cherry_red.jpg"},
-            {value: "Emerald Green", image: "/Hyperion_Halo/emerald_green.jpg"},
-            {value: "Ice Blue", image: "/Hyperion_Halo/ice_blue.jpg"}
-        ], tires, rims
-    }
-    const Traverse = {
-        colors:[
-            {value: "Space Grey", image: "/Hyperion_Halo/space_grey.jpg"},
-            {value: "Cherry Red", image: "/Hyperion_Halo/cherry_red.jpg"},
-            {value: "Emerald Green", image: "/Hyperion_Halo/emerald_green.jpg"},
-            {value: "Ice Blue", image: "/Hyperion_Halo/ice_blue.jpg"}
-        ], tires, rims       
-    }
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    if(car == "HyperionHalo"){
-        res.json(HyperionHalo)
-    } else if(car == "Traverse"){
-        res.json(Traverse)
-    } else {
-        res.sendStatus(404)
-    }*/
 })
 
 const corsOptions = {
