e6121b6c9902cab48cde7a1892dbe5ed35a31927
Vincent <vincentmattes@gmail.com>
2024-07-02 17:27:42 +0200

    trying to fix testdrive service with database access

--- business/testdriveService/default.js
+++ business/testdriveService/default.js
@@ -11,7 +11,7 @@ const PORT = 82
 
 app.get("/", async function (req,res){
     const connection = await mysql.createConnection({
-        host: 'localhost',
+        host: 'car_db',
         user: 'hhz',
         password: 'car',
         database: 'merchandise_db'
--- presentation/default.js
+++ presentation/default.js
@@ -14,7 +14,7 @@ const PORT = 80
 
 app.get("/", async function (req,res){
     //get location from microservice
-    const response = await fetch("http://localhost:81")
+    const response = await fetch("http://locationservice:81")
     const jsonresponse = await response.json()
     console.log(jsonresponse)
     res.render("home.ejs", { 
@@ -28,13 +28,13 @@ app.get("/models", function (req, res){
 })
 
 app.get("/configuratorHyperionHalo", async function (req, res){
-    const response = await fetch("http://localhost:86/HyperionHalo")
+    const response = await fetch("http://configuratorservice:86/HyperionHalo")
     const jsonresponse = await response.json()
     res.render("configuratorHyperionHalo.ejs", { showNavbar: true, configuration: jsonresponse })
 })
 
 app.get("/configuratorTraverse", async function (req, res){
-    const response = await fetch("http://localhost:86/Traverse")
+    const response = await fetch("http://configuratorservice:86/Traverse")
     const jsonresponse = await response.json()
     res.render("configuratorTraverse.ejs", { showNavbar: true, configuration: jsonresponse })
 })
@@ -51,13 +51,13 @@ app.get("/configurator/:model", function (req, res){
 })
 
 app.get("/testdrive", async function (req, res){
-    const response = await fetch("http://localhost:82")
+    const response = await fetch("http://testdriveservice:82")
     const jsonresponse = await response.json()
     res.render("testdrive.ejs", { showNavbar: true, carsAndTracks: jsonresponse })
 })
 
 app.get("/merch", async function (req, res){
-    const response = await fetch("http://localhost:85")
+    const response = await fetch("http://merchitemsservice:85")
     const jsonresponse = await response.json()
     res.render("merchshop.ejs", { showNavbar: true, merchItems: jsonresponse })
 })
